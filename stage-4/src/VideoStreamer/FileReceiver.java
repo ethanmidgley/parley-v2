bcf481e7fc1da0f1adefe6976ab121dd82321478
fqb22153 <kieran.ballard.2022@uni.strath.ac.uk>
2025-03-26 16:54:46 +0000

    stupid SOL removed

--- stage-4/src/VideoStreamer/FileReceiver.java
+++ stage-4/src/VideoStreamer/FileReceiver.java
@@ -30,7 +30,6 @@ public class FileReceiver {
     player.start();
     vs = new VideoStreamer(peer,PORT_NUMBER,(VideoAudioPair vap) -> {
       if (vap.video.length > 0) {
-        // System.out.println("hasdlfkjasd;lfkjsda;lkj");
       }
       player.addFrame(vap);
     });
