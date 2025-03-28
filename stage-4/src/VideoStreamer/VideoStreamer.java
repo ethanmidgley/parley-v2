69733c4b025a964cac2b61dbbb7e7238ab24c6e2
MuxDiven <divenmax15@gmail.com>
2025-03-28 18:35:36 +0000

    fixed stupid shit i think

--- stage-4/src/VideoStreamer/VideoStreamer.java
+++ stage-4/src/VideoStreamer/VideoStreamer.java
@@ -123,18 +123,16 @@ public class VideoStreamer extends Thread {
    socket.close();
    System.out.println("sending termination to file streamer, file receiver line 45");
    //FIXME? this shit might fail if running is set to false form somewhere else
-   if(running.get()) {
-     try {
-       //account for peer being null
-       DatagramSocket dgs = new DatagramSocket(11000);
-       DatagramPacket dap = new DatagramPacket(new byte[255], 255,peer,TERMINATION_PORT_NUMBER);
-       dgs.send(dap);
-       dgs.close();
-     } catch (SocketException e) {
-       throw new RuntimeException(e);
-     } catch (IOException e) {
-       throw new RuntimeException(e);
-     }
+   try {
+     //account for peer being null
+     DatagramSocket dgs = new DatagramSocket(11000);
+     DatagramPacket dap = new DatagramPacket(new byte[255], 255,peer,TERMINATION_PORT_NUMBER);
+     dgs.send(dap);
+     dgs.close();
+   } catch (SocketException e) {
+     throw new RuntimeException(e);
+   } catch (IOException e) {
+     throw new RuntimeException(e);
    }
    this.running.set(false);
   }
